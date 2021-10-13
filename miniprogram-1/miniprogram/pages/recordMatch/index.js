249b8ac79dff34a8b0aff5b280a90d8b459d22ae
Guanhua Mai <guamai@microsoft.com>
2021-10-13 14:14:06 +0800

    Add start games.

--- miniprogram-1/miniprogram/pages/recordMatch/index.js
+++ miniprogram-1/miniprogram/pages/recordMatch/index.js
@@ -1,99 +1,178 @@
 // pages/recordMatch/index.js
 Page({
 
-    /**
-     * Page initial data
-     */
-    data: {
-        games : [],
-        envId: "",
-        matchId: ""
-    },
+  /**
+   * Page initial data
+   */
+  data: {
+    games: [],
+    envId: "",
+    matchId: ""
+  },
 
-    bindKeyInput: function(e){
-      this.setData({matchId: e.detail.value})
-    },
+  bindKeyInput: function (e) {
+    this.setData({ matchId: e.detail.value })
+  },
 
-    onStartMatch: function (options) {
-      wx.cloud.callFunction({
-        name: 'quickstartFunctions',
-        config: {
-          env: this.data.envId
-        },
-        data: {
-          type: 'getMatchForRecording',
-          matchId: this.data.matchId
-        }
-      }).then((resp) => {
-        console.log("response: ", resp)
-        this.setData({
-          games: resp.result.data
-        })
-       wx.hideLoading()
-     }).catch((e) => {
-        this.setData({
-          showUploadTip: true
-        })
-        console.log("exception-", e)
-       wx.hideLoading()
-      })
-    },
-
-    /**
-     * Lifecycle function--Called when page load
-     */
-    onLoad: function (options) {
+  onStartMatch: function (options) {
+    wx.cloud.callFunction({
+      name: 'quickstartFunctions',
+      config: {
+        env: this.data.envId
+      },
+      data: {
+        type: 'getMatchForRecording',
+        matchId: this.data.matchId
+      }
+    }).then((resp) => {
+      console.log("response: ", resp)
+      var selectedMatch = resp.result.data[0]
+      console.log(selectedMatch)
+      var generatedGames = generateGames(selectedMatch.participants, this.data.matchId)
+      console.log("generated games:", generatedGames)
       this.setData({
-        envId: options.envId
+        games: generatedGames
       })
-    },
+      wx.hideLoading()
+    }).catch((e) => {
+      this.setData({
+        showUploadTip: true
+      })
+      console.log("exception-", e)
+      wx.hideLoading()
+    })
+  },
 
-    /**
-     * Lifecycle function--Called when page is initially rendered
-     */
-    onReady: function () {
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    this.setData({
+      envId: options.envId
+    })
+  },
 
-    },
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
 
-    /**
-     * Lifecycle function--Called when page show
-     */
-    onShow: function () {
+  },
 
-    },
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
 
-    /**
-     * Lifecycle function--Called when page hide
-     */
-    onHide: function () {
+  },
 
-    },
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
 
-    /**
-     * Lifecycle function--Called when page unload
-     */
-    onUnload: function () {
+  },
 
-    },
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
 
-    /**
-     * Page event handler function--Called when user drop down
-     */
-    onPullDownRefresh: function () {
+  },
 
-    },
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
 
-    /**
-     * Called when page reach bottom
-     */
-    onReachBottom: function () {
+  },
 
-    },
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
 
-    /**
-     * Called when user click on the top right corner to share
-     */
-    onShareAppMessage: function () {
+  },
 
-    }
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
 })
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function generateGames(array, matchId) {
+  var result = []
+  if (array.length === 5) {
+    var a = shuffle(array)
+    console.log("shuffled array", a)
+    result = [
+      {
+        "left1": a[0],
+        "left2": a[1],
+        "leftScore": 0,
+        "right1": a[2],
+        "right2": a[3],
+        "rightScore": 0,
+        "matchId": matchId,
+        "number": 1
+      }, {
+        "left1": a[2],
+        "left2": a[4],
+        "leftScore": 0,
+        "right1": a[1],
+        "right2": a[3],
+        "rightScore": 0,
+        "matchId": matchId,
+        "number": 2
+      }, {
+        "left1": a[1],
+        "left2": a[4],
+        "leftScore": 0,
+        "right1": a[0],
+        "right2": a[3],
+        "rightScore": 0,
+        "matchId": matchId,
+        "number": 3
+      }, {
+        "left1": a[1],
+        "left2": a[2],
+        "leftScore": 0,
+        "right1": a[0],
+        "right2": a[4],
+        "rightScore": 0,
+        "matchId": matchId,
+        "number": 4
+      }, {
+        "left1": a[0],
+        "left2": a[2],
+        "leftScore": 0,
+        "right1": a[3],
+        "right2": a[4],
+        "rightScore": 0,
+        "matchId": matchId,
+        "number": 5
+      }]
+    console.log("Generated games: ", result)
+  }
+  return result
+}
