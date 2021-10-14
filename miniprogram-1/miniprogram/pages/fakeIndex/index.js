353e0758c18487f308e19cea09d874eb9491d1c7
Zhuosi Xie <zhuxie@microsoft.com>
2021-10-14 14:55:52 +0800

    fake index

---
+++ miniprogram-1/miniprogram/pages/fakeIndex/index.js
@@ -0,0 +1,127 @@
+//index.js
+const app = getApp()
+const { envList } = require('../../envList.js')
+
+Page({
+  data: {
+    showUploadTip: false,
+    nickName: '',
+    powerList: [{
+      title: '羽毛球',
+      tip: '乐子大师为您服务',
+      showItem: false,
+      item: [{
+        title: '看看世界排名',
+        page: 'badminton'
+      },
+      {
+        title: '发起羽毛球比赛',
+        page: 'createMatch'
+      },
+      {
+        title: '加入羽毛球比赛',
+        page: 'joinMatch'
+      },
+      {
+        title: '记录羽毛球比赛',
+        page: 'recordMatch'
+      }
+    ]
+    }],
+    envList,
+    selectedEnv: envList[0],
+    haveCreateCollection: false
+  },
+
+  onClickPowerInfo(e) {
+    const index = e.currentTarget.dataset.index
+    const powerList = this.data.powerList
+    powerList[index].showItem = !powerList[index].showItem
+    if (powerList[index].title === '数据库' && !this.data.haveCreateCollection) {
+      this.onClickDatabase(powerList)
+    } else {
+      this.setData({
+        powerList
+      })
+    }
+  },
+
+  onChangeShowEnvChoose() {
+    wx.showActionSheet({
+      itemList: this.data.envList.map(i => i.alias),
+      success: (res) => {
+        this.onChangeSelectedEnv(res.tapIndex)
+      },
+      fail (res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
+
+  onChangeSelectedEnv(index) {
+    if (this.data.selectedEnv.envId === this.data.envList[index].envId) {
+      return
+    }
+    const powerList = this.data.powerList
+    powerList.forEach(i => {
+      i.showItem = false
+    })
+    this.setData({
+      selectedEnv: this.data.envList[index],
+      powerList,
+      haveCreateCollection: false
+    })
+  },
+
+  jumpPage(e) {
+    if (e.currentTarget.dataset.page == 'badminton'){
+      wx.navigateTo({
+        url: `/pages/${e.currentTarget.dataset.page}/index?envId=${this.data.selectedEnv.envId}`,
+      })
+    }else{
+      wx.navigateTo({
+        url: `/pages/index/index?envId=${this.data.selectedEnv.envId}`,
+      })
+    }
+    
+  },
+
+  onLoad: function(options) {
+    this.setData({
+      nickName: options.nickName
+    })
+
+    console.log(this.nickName, options)
+  },
+
+  onClickDatabase(powerList) {
+    wx.showLoading({
+      title: '',
+    })
+    wx.cloud.callFunction({
+      name: 'quickstartFunctions',
+      config: {
+        env: this.data.selectedEnv.envId
+      },
+      data: {
+        type: 'createCollection'
+      }
+    }).then((resp) => {
+      if (resp.result.success) {
+        this.setData({
+          haveCreateCollection: true
+        })
+      }
+      this.setData({
+        powerList
+      })
+      wx.hideLoading()
+    }).catch((e) => {
+      console.log(e)
+      this.setData({
+        showUploadTip: true
+      })
+      wx.hideLoading()
+    })
+  }
+})
